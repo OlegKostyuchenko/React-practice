a87decff7199ddb9185f7acbcaa424fb3fa66e55
Oleg <mr.lost@rambler.ru>
2022-05-18 17:34:15 +0300

    Add func DeleteItem, AddItem, like, favorite

--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -2,12 +2,22 @@ import React from "react";
 import './post-list.css'
 
 import PostListItem from '../post-list-item';
-const PostList = () => {
+const PostList = ({ posts, onDelete }) => {
+
+    const elements = posts.map((item) => {
+        const { id, ...itemProps } = item;
+        return (
+            <li key={id} className='list-group-item'>
+                <PostListItem
+                    {...itemProps}
+                    onDelete={() => onDelete(id)} />
+            </li>
+        )
+    });
+
     return (
         <ul className="app-list list-group">
-            <PostListItem />
-            <PostListItem />
-            <PostListItem />
+            {elements}
         </ul>
     )
 }
